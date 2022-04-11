9e2c1882db17e29ccce2d667294d1a797f052ae3
sanselin <oscarsial@gmail.com>
2022-04-10 21:58:18 -0500

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -6,6 +6,21 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const getLs = () => {
+      const pacientesLs = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+      setPacientes(pacientesLs);
+    };
+    getLs();
+  }, []);
+
+  useEffect(() => {
+    const setPacientesLs = () => {
+      localStorage.setItem("pacientes", JSON.stringify(pacientes));
+    };
+    setPacientesLs();
+  }, [pacientes]);
 
   const deletePacientes = (id) => {
     const filterPacientes = pacientes.filter((paciente) => paciente.id !== id);
